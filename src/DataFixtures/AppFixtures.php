a905a1747894ef0710aa0a8b9b21d2f2b6f755fe
burakovas <burakovas@gmail.com>
2019-08-21 21:17:01 +0300

    project fixtures add

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Project;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,31 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadProjects($manager);
+
+    }
+
+    private function loadProjects($manager)
+    {
+        foreach ($this->getProjectsData() as $name){
+            $project = new Project();
+            $project->setName($name);
+            $project->setDate(\DateTime::createFromFormat('Y-m-d', "2018-09-09"));
+            $project->setCategory(1);
+            $project->setDescription("some Description");
+            $project->setInvite(1);
+
+            $manager->persist($project);
+        }
 
         $manager->flush();
+
+    }
+
+
+    private function getProjectsData() {
+
+        return ['First Project', 'Task Tracker', 'Some project'];
+
     }
 }
